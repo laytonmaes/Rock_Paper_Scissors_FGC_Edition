ce8c178f150a49795e2796f73b027924864939ac
Dylan Duke <laytonmaes@gmail.com>
2022-03-03 19:39:48 -0700

    Finish base ui, add rule select section

--- scripts.js
+++ scripts.js
@@ -1,28 +1,28 @@
-playerOne = new Player("Player One")
-computerPlayer = new Player("CPU")
-
-weaponOptions = ["dragon punch", "heavy punch", "tornado kick", "fire ball", "heavy kick"]
-
-function getRandomIndex(array) {
-  return Math.floor (Math.random() * array.length)
-}
-
-function getRandomWeapon() {
-  weaponOptions[getRandomIndex(weaponOptions)]
-}
-
-function playSimpleRPS() {
-  playerOne.weaponSelection = getRandomWeapon();
-  computerPlayer.weaponSelection = getRandomWeapon();
-  evaluateSimpleRps()
-}
-
-function evaluateSimpleRps(){
-
-}
-function playComplexRPS() {
-
-}
-function dealDamage() {
-  
-}
+// playerOne = new Player("Player One")
+// computerPlayer = new Player("CPU")
+//
+// weaponOptions = ["dragon punch", "heavy punch", "tornado kick", "fire ball", "heavy kick"]
+//
+// function getRandomIndex(array) {
+//   return Math.floor (Math.random() * array.length)
+// }
+//
+// function getRandomWeapon() {
+//   weaponOptions[getRandomIndex(weaponOptions)]
+// }
+//
+// function playSimpleRPS() {
+//   playerOne.weaponSelection = getRandomWeapon();
+//   computerPlayer.weaponSelection = getRandomWeapon();
+//   evaluateSimpleRps()
+// }
+//
+// function evaluateSimpleRps(){
+//
+// }
+// function playComplexRPS() {
+//
+// }
+// function dealDamage() {
+//
+// }
